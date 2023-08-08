c8efd6863f344d4a45e57f1379941af657da798c
brucetravis <bruceambundo@gmail.com>
2023-08-08 02:03:48 -0700

    loops

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+def uppercase(str):
+    for char in str:
+        ascii_value = ord(char)
+        if 97 <= ascii_value <= 122:
+            ascii_value -= 32
+        print("{:c}".format(ascii_value), end="")
+    print()
+
