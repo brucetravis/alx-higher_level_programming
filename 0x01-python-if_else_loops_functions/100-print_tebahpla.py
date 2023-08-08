fc457211896d46202ae7b38269808a7e4dc63527
brucetravis <bruceambundo@gmail.com>
2023-08-08 02:17:43 -0700

    100_print

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
 for i in range(122, 96, -1):
-    print("{:c}".format(i), end="")
-    if i - 1 >= 97:
-        print("{:c}".format(i - 33), end="")
+    print("{:c}".format(i - 32)
+            if i % 2 == 1 else "{:c}".format(i), end="")
