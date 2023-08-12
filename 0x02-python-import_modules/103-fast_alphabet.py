449e87ea735e86f95714d6e16cc5b5392e7e57c8
brucetravis <bruceambundo@gmail.com>
2023-08-12 02:53:38 -0700

    103-fast_alphabet.py

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,2 +1,3 @@
 #!/usr/bin/python3
-__import__('sys').stdout.write(bytes(range(65, 91)).decode('utf-8') + '\n')
+from string import ascii_uppercase
+__import__('sys').stdout.write(ascii_uppercase + '\n')
