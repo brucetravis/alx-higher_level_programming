a706b611d9d36176bda492749a9bdb990585d53d
brucetravis <bruceambundo@gmail.com>
2023-08-30 02:33:10 -0700

    101-square

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -13,7 +13,19 @@ class Square:
         size (int): The size of the square's sides.
         position (tuple): The position of the square's top-left corner.
     """
+
     def __init__(self, size=0, position=(0, 0)):
+        """
+        Initialize a new Square instance.
+
+        Args:
+            size (int, optional): The size of the square's sides (default is 0).
+            position (tuple, optional): The position of the square's top-left corner (default is (0, 0)).
+
+        Raises:
+            TypeError: If size is not an integer or position is not a tuple.
+            ValueError: If size is less than 0 or position values are less than 0.
+        """
         self.size = size
         self.position = position
 
@@ -41,9 +53,18 @@ class Square:
         self.__position = value
 
     def area(self):
+        """
+        Calculate the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
         return self.__size ** 2
 
     def my_print(self):
+        """
+        Print the square using '#' characters.
+        """
         if self.__size == 0:
             print()
             return
@@ -54,16 +75,23 @@ class Square:
             print("#" * self.__size)
 
     def __str__(self):
-        result = []
-        if self.__size == 0:
-            result.append("")
-        else:
-            for _ in range(self.__position[1]):
-                result.append("\n")
-            for _ in range(self.__size):
-                result.append(" " * self.__position[0] + "#" * self.__size)
+        """
+        String representation of the Square instance.
 
-        return ''.join(result).strip()
+        Returns:
+            str: Formatted string representation.
+        """
+        result = ""
+        if self.__size == 0:
+            return result
+
+        for _ in range(self.__position[1]):
+            result += "\n"
+        for _ in range(self.__size):
+            result += " " * self.__position[0] + "#" * self.__size + "\n"
+
+        return result.rstrip("\n")
+
 
 if __name__ == "__main__":
     my_square = Square(5, (0, 0))
