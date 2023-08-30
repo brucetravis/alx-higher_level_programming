b4370c0b48ff6661e0387a6c2a87ff140d1b3aaf
brucetravis <bruceambundo@gmail.com>
2023-08-29 22:36:35 -0700

    number 2 commit

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,21 +1,57 @@
 #!/usr/bin/python3
 
-"""This module defines a Square class."""
-
-
 class Square:
     """
     A class to represent a square.
-    
+
     Attributes:
         __size (int): The size of the square.
     """
-    
-    def __init__(self, size):
+
+    def __init__(self, size=0):
         """
         Initialize a new Square instance.
-        
+
         Args:
             size (int): The size of the square.
         """
         self.__size = size
+
+        if type(self.__size) is not int:
+            raise TypeError("size must be an integer")
+        elif self.__size < 0:
+            raise ValueError("size must be >= 0")
+
+
+if __name__ == "__main__":
+    my_square_1 = Square(3)
+    print(type(my_square_1))
+    print(my_square_1.__dict__)
+
+    my_square_2 = Square()
+    print(type(my_square_2))
+    print(my_square_2.__dict__)
+
+    try:
+        print(my_square_1.size)
+    except Exception as e:
+        print(e)
+
+    try:
+        print(my_square_1.__size)
+    except Exception as e:
+        print(e)
+
+    try:
+        my_square_3 = Square("3")
+        print(type(my_square_3))
+        print(my_square_3.__dict__)
+    except Exception as e:
+        print(e)
+
+    try:
+        my_square_4 = Square(-89)
+        print(type(my_square_4))
+        print(my_square_4.__dict__)
+    except Exception as e:
+        print(e)
