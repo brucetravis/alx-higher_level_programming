8bf513f8e4e0998f63ef54ca46c669495095f719
brucetravis <bruceambundo@gmail.com>
2023-08-29 22:28:14 -0700

    6 update

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 
+"""This module defines a Square class."""
+
+
 class Square:
+    """A class representing a square."""
+
     def __init__(self, size=0, position=(0, 0)):
         self.size = size
         self.position = position
@@ -23,8 +28,8 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if not isinstance(value, tuple) or len(value) != 2 or \
-           not all(isinstance(v, int) and v >= 0 for v in value):
+        if (not isinstance(value, tuple) or len(value) != 2 or
+                not all(isinstance(v, int) and v >= 0 for v in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
@@ -40,3 +45,18 @@ class Square:
         for _ in range(self.__size):
             print(" " * self.__position[0], end="")
             print("#" * self.__size)
+
+
+if __name__ == "__main__":
+    my_square_1 = Square(3)
+    my_square_1.my_print()
+
+    print("--")
+
+    my_square_2 = Square(3, (1, 1))
+    my_square_2.my_print()
+
+    print("--")
+
+    my_square_3 = Square(3, (3, 0))
+    my_square_3.my_print()
