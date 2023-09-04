2a24d139a1c1c43b52a941ad327a14390baa403a
brucetravis <bruceambundo@gmail.com>
2023-09-04 04:38:14 -0700

    first commit

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
 
 class Rectangle:
+    """
+    This is a simple Rectangle class.
+
+    It can be used to represent rectangles with width and height attributes.
+
+    Attributes:
+        width (int): The width of the rectangle.
+        height (int): The height of the rectangle.
+    """
     pass
