a7ffb84dbedddd956795633acc3de53d172e251b
brucetravis <bruceambundo@gmail.com>
2023-09-04 21:27:38 -0700

    0-rectangle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,20 +1,9 @@
 #!/usr/bin/python3
 
+"""
+This module defines the Rectangle class.
+"""
+
+
 class Rectangle:
-    """
-    This is a simple Rectangle class.
-
-    It can be used to represent rectangles with width and height attributes.
-
-    Attributes:
-        width (int): The width of the rectangle.
-        height (int): The height of the rectangle.
-    """
     pass
-
-
-if __name__ == "__main__":
-    # Example usage:
-    my_rectangle = Rectangle()
-    print(type(my_rectangle))
-    print(my_rectangle.__dict__)
