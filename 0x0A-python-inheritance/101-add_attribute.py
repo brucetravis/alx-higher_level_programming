23f5a92ee9767bf9055f7ff38146a2fd6cd19ab5
brucetravis <bruceambundo@gmail.com>
2023-09-11 14:19:18 -0700

    last commit

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -12,7 +12,8 @@ def add_attribute(obj, attribute, value):
     Raises:
         TypeError: If the object cannot have a new attribute.
     """
-    if hasattr(obj, "__dict__") or (hasattr(obj, "__slots__") and attribute in obj.__slots__):
+    if hasattr(obj, "__dict__") or (hasattr(obj, "__slots__") and
+                                    attribute in obj.__slots__):
         setattr(obj, attribute, value)
     else:
         raise TypeError("can't add new attribute")
