bae34df534210a2ba6fd0e4e016264ba484bc3c4
brucetravis <bruceambundo@gmail.com>
2023-09-12 08:05:52 -0700

    5 commit

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -14,5 +14,5 @@ def save_to_json_file(my_obj, filename):
     Returns:
         None
     """
-    with open(filename, 'w', encoding='utf-8') as file:
-        json.dump(my_obj, file)
+    with open(filename, "w") as f:
+        json.dump(my_obj, f)
