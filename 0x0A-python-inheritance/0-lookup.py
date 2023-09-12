1d29f7f5f7bd9a260a3993b8aa982981f3e3b66b
brucetravis <bruceambundo@gmail.com>
2023-09-11 21:25:40 -0700

    1 commit

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -11,3 +11,40 @@ def lookup(obj):
         list: A list of strings representing the attributes and methods.
     """
     return dir(obj)
+
+
+# Test cases
+if __name__ == "__main__":
+    print("Attributes and methods of int:")
+    print(lookup(int))
+
+    print("\nAttributes and methods of float:")
+    print(lookup(float))
+
+    print("\nAttributes and methods of object:")
+    print(lookup(object))
+
+    print("\nAttributes and methods of list:")
+    print(lookup(list))
+
+    class MyClass:
+        def dir(self):
+            return ["my_class", "is", "empty"]
+
+    print("\nAttributes and methods of MyClass with custom dir method:")
+    my_instance = MyClass()
+    print(lookup(my_instance))
+
+    class MyClassWithAttributes:
+        one_attribute = 89
+
+    print("\nAttributes and methods of MyClassWithAttributes:")
+    print(lookup(MyClassWithAttributes))
+
+    class MyClassWithMethod:
+        def one_meth(self):
+            pass
+
+    print("\nAttributes and methods of MyClassWithMethod:")
+    my_instance_with_method = MyClassWithMethod()
+    print(lookup(my_instance_with_method))
