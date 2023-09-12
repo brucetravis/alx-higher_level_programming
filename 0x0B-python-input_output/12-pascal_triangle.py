0513e9293a481a088f826a0d6f620a6890d9e05b
brucetravis <bruceambundo@gmail.com>
2023-09-12 07:39:41 -0700

    12 commit

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+
+"""
+This module defines a function to generate Pascal's triangle.
+"""
+
+
+def pascal_triangle(n):
+    """
+    Generate Pascal's triangle up to the nth row.
+
+    Args:
+        n (int): The number of rows to generate.
+
+    Returns:
+        list of lists: A list of lists representing Pascal's triangle.
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        row = [1]
+        prev_row = triangle[i - 1]
+
+        for j in range(1, i):
+            row.append(prev_row[j - 1] + prev_row[j])
+
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
