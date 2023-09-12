f1daabc1ebedb6b23b6b57442c83bb8522698860
brucetravis <bruceambundo@gmail.com>
2023-09-12 06:06:13 -0700

    8-commit
    \
    ]

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python3
+"""Defines a class Student."""
+
+
 def class_to_json(obj):
     """
     Converts an instance of a class to a JSON-serializable dictionary.
@@ -8,20 +12,5 @@ def class_to_json(obj):
     Returns:
         dict: A dictionary containing the serialized attributes of the object.
     """
-    # Initialize an empty dictionary to store the serialized data
-    serialized_data = {}
 
-    # Get the list of attributes of the object
-    attributes = dir(obj)
-
-    for attr in attributes:
-        # Check if the attribute is not a special method (e.g., __init__)
-        if not attr.startswith('__'):
-            # Get the attribute value
-            attr_value = getattr(obj, attr)
-
-            # Check if the attribute is serializable (list, dict, str, int, bool)
-            if isinstance(attr_value, (list, dict, str, int, bool)):
-                serialized_data[attr] = attr_value
-
-    return serialized_data
+    return obj.__dict__
