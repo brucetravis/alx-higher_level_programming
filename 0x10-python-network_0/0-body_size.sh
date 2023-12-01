d5262d4e12501fa1df0b119d1a7b7fc66655febf
brucetravis <bruceambundo@gmail.com>
2023-11-30 21:56:42 -0800

    2 commits

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,9 +2,10 @@
 
 # Check if URL argument is provided
 if [ -z "$1" ]; then
+    # Print usage message if URL is not provided
     echo "Usage: $0 <URL>"
     exit 1
 fi
 
-# Use curl to send a request and display the size of the body in bytes
-curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
+# Use curl to send a request and display the size of the body in bytes without headers
+curl -s "$1" | wc -c
--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -2,14 +2,16 @@
 
 # Check if URL argument is provided
 if [ -z "$1" ]; then
+    # Print usage message if URL is not provided
     echo "Usage: $0 <URL>"
     exit 1
 fi
 
-# Use curl to send a GET request and display the body if status code is 200
+# Use curl to send a GET request and display "Route 2" if the status code is 200
 curl -s -o /dev/null -w "%{http_code}" "$1" | {
     read status
     if [ "$status" -eq 200 ]; then
-        curl -s "$1"
+        # Output "Route 2"
+        echo "Route 2"
     fi
 }
